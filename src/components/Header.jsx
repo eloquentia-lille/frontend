a50c7b75397b8e64f21082e5218404712e91b380
Luissignol <r.pasquier6@gmail.com>
2026-06-18 17:50:23 +0200

    feat: begin of responsive when width > 900px, menu still on when mobie screen

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,57 +1,69 @@
 import { NavLink } from "react-router-dom";
 import logo from "../assets/temporaire_logo.PNG";
+import menuLogo from "../assets/temp_menu_logoPNG.PNG";
 import "../styles/Header.css";
 
 function Header() {
     return(
         <header>
-            <nav>
-
-                <NavLink to="/">
-                    <img className="logo" src={logo} alt="logo-eloquentia" />
-                </NavLink>
-
+            <div className="logoContainer">
                 <NavLink 
-                to="/"
-                className={({ isActive }) =>
-                    isActive ? "activeLink" : "navLink"
-                }>
-                    Accueil
+                    to="/"
+                    className={({ isActive }) =>
+                        isActive ? "logoButton activeLink" : "logoButton navLink"
+                    }>
+                        <img className="logo" src={logo} alt="logo-eloquentia" />
                 </NavLink>
 
-                <NavLink 
-                to="/calendrier"
-                className={({ isActive }) =>
-                    isActive ? "activeLink" : "navLink"
-                }>
-                    Calendrier
-                </NavLink>
-
-                <NavLink 
-                to="/actualite"
-                className={({ isActive }) =>
-                    isActive ? "activeLink" : "navLink"
-                }>
-                    Actualité
-                </NavLink>
-
-                <NavLink 
-                to="/galerie"
-                className={({ isActive }) =>
-                    isActive ? "activeLink" : "navLink"
-                }>
-                    Galerie
-                </NavLink>
-
-                <NavLink 
-                to="/contact"
-                className={({ isActive }) =>
-                    isActive ? "activeLink" : "navLink"
-                }>
-                    Contact
-                </NavLink>
-                
-            </nav>
+                <button className="menuButton">
+                    <img className="menuLogo" src={menuLogo} alt="menu-logo" />
+                </button>
+            </div>
+            
+            <div className="mobileNav">
+                    <nav>
+                        <NavLink 
+                        to="/"
+                        className={({ isActive }) =>
+                            isActive ? "activeLink" : "navLink"
+                        }>
+                            Accueil
+                        </NavLink>
+                    
+                        <NavLink 
+                        to="/calendrier"
+                        className={({ isActive }) =>
+                            isActive ? "activeLink" : "navLink"
+                        }>
+                            Calendrier
+                        </NavLink>
+                    
+                        <NavLink 
+                        to="/actualite"
+                        className={({ isActive }) =>
+                            isActive ? "activeLink" : "navLink"
+                        }>
+                            Actualité
+                        </NavLink>
+                    
+                        <NavLink 
+                        to="/galerie"
+                        className={({ isActive }) =>
+                            isActive ? "activeLink" : "navLink"
+                        }>
+                            Galerie
+                        </NavLink>
+                    
+                        <NavLink 
+                        to="/contact"
+                        className={({ isActive }) =>
+                            isActive ? "activeLink" : "navLink"
+                        }>
+                            Contact
+                        </NavLink>
+                </nav>
+            </div>
+            
 
         </header>
     );
