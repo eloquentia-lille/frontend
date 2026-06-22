0bb669f0b891757327306283c178454a5856d0a0
Luissignol <r.pasquier6@gmail.com>
2026-06-22 10:13:03 +0200

    feat: addition of accessibility arias on toggle and navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
-import { NavLink } from "react-router-dom";
-import logo from "../assets/temporaire_logo.PNG";
-import menuLogo from "../assets/temp_menu_logoPNG.PNG";
 import "../styles/Header.css";
 
+import { NavLink } from "react-router-dom";
 import { useState } from "react";
 
+
+import logo from "../assets/temporaire_logo.PNG";
+import menuLogo from "../assets/temp_menu_logoPNG.PNG";
 
 
 function Header() {
@@ -16,59 +17,68 @@ function Header() {
     return(
         <header>
             <div className="logoContainer">
+
                 <NavLink 
                     to="/"
                     className={({ isActive }) =>
                         isActive ? "logoButton activeLink" : "logoButton navLink"
                     }>
-                        <img className="logo" src={logo} alt="logo-eloquentia" />
+                        <img className="logo" src={logo} alt="Logo de l'association Eloquentia" />
                 </NavLink>
 
                 <button 
                 onClick={toggle} 
+                className="menuButton"
                 aria-label="Ouvrir le menu"
-                className="menuButton">
-                    <img className="menuLogo" src={menuLogo} alt="menu-logo" />
+                aria-expanded={show}
+                aria-controls="main-menu"
+                >
+                    <img 
+                    className="menuLogo" 
+                    src={menuLogo} 
+                    alt="Logo du menu"
+                    aria-hidden="true" />
                 </button>
+
             </div>
 
             <div 
             className={show ? "menuOn" : "menuOff"}
             >
-                    <nav>
-                        <NavLink 
-                        to="/"
-                        className={navClass}>
-                            Accueil
-                        </NavLink>
-                    
-                        <NavLink 
-                        to="/calendrier"
-                        className={navClass}>
-                            Calendrier
-                        </NavLink>
-                    
-                        <NavLink 
-                        to="/actualite"
-                        className={navClass}>
-                            Actualité
-                        </NavLink>
-                    
-                        <NavLink 
-                        to="/galerie"
-                        className={navClass}>
-                            Galerie
-                        </NavLink>
-                    
-                        <NavLink 
-                        to="/contact"
-                        className={navClass}>
-                            Contact
-                        </NavLink>
+                <nav 
+                aria-label="Navigation principale"
+                id="main-menu">
+                    <NavLink 
+                    to="/"
+                    className={navClass}>
+                        Accueil
+                    </NavLink>
+                
+                    <NavLink 
+                    to="/calendrier"
+                    className={navClass}>
+                        Calendrier
+                    </NavLink>
+                
+                    <NavLink 
+                    to="/actualite"
+                    className={navClass}>
+                        Actualité
+                    </NavLink>
+                
+                    <NavLink 
+                    to="/galerie"
+                    className={navClass}>
+                        Galerie
+                    </NavLink>
+                
+                    <NavLink 
+                    to="/contact"
+                    className={navClass}>
+                        Contact
+                    </NavLink>
                 </nav>
             </div>
-            
-
         </header>
     );
 }
