33770bbd647061b71fa28c9f61614a5da19d2d3c
Luissignol <r.pasquier6@gmail.com>
2026-06-25 13:10:04 +0200

    feat: gallery page done using card component with different css sheet

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,31 +1,44 @@
-import "../styles/CardActuality.css";
-
 function Card(props) {
     let image = null;
 
     if(props.img) {
         image = (
             <img 
-            src={props.img} 
+            src={props.img}
+            aria-label={props.label}
             alt={props.imgAlt}
             />
         );
     }
     else{
-        image = (<div className="no-img"></div>)
+        image = (<div className={`no-img ${props.variant}`}></div>)
     }
 
     return (
         <>
-            <section className="card">
-                <div className="img-div">
+            <section className={`card ${props.variant}`}>
+
+                <div className={`img-div ${props.variant}`}>
                     {image}
                 </div>
-                <div className="text-div">
-                    <h2 className="card-title">{props.titre}</h2>
-                    <p className="card-date">Publié le {props.date}</p>
-                    <p className="card-description">{props.description}</p>
+
+                <div className={`text-div ${props.variant}`}>
+                    <h2 className={`card-title ${props.variant}`}>{props.titre}</h2>
+
+                    <p className={`card-date ${props.variant}`}>Publié le {props.date}</p>
+                    
+                    <p className={`card-description ${props.variant}`}>{props.description}</p>
                 </div>
+
+                {props.variant === "gallery" && (
+                <div className="btn-div">
+                    <button 
+                    className="btn-access"
+                    onClick={() => window.alert("La galerie est affichée !")}
+                    >
+                        Voir galerie</button>
+                </div>
+                )}
             </section>
         </>
     );
